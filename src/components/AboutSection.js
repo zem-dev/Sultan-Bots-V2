217f210ffdf2d468cd951ce6b6d19eaa8cbbee25
egg <eggboissaye@gmail.com>
2022-06-10 16:47:44 -0400

    Created about section, plans & pricing section, and contact section

---
+++ src/components/AboutSection.js
@@ -0,0 +1,12 @@
+import "../css/about-section-styles.css"
+
+const AboutSection = () => {
+    return (
+        <div className="about-section">
+            <h1 className="about-section-title">Custom Bot for <span className="your-server-span">Your Server</span></h1>
+            <p className="about-section-desc">Just tell us what you need and we’ll get it done. From utility to just fun commands, we can make bots equipped for any server.</p>
+        </div>
+    )
+}
+
+export default AboutSection;
--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,5 +1,8 @@
 import HeroArea from "../HeroArea";
 import HomeNavbar from "../Navbar";
+import AboutSection from "../AboutSection";
+import PlansSection from "../PlansSection";
+import ContactSection from "../ContactSection";
 
 import "../../css/home-page-styles.css"
 
@@ -8,6 +11,9 @@ const HomePage = () => {
         <>
             <HomeNavbar/>
             <HeroArea/>
+            <AboutSection/>
+            <PlansSection/>
+            <ContactSection/>
         </>
     )
 }
